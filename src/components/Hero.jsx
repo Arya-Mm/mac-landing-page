51afc68b7e3cd5b8bf5d2b4a16098e7dc47bd667
Arya-Mm <aryamishra828@gmail.com>
2026-03-15 05:55:17 +0000

    Mar 15, 2026, 11:25 AM

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id = "hero">
+        <div>
+            <h1>MacBook Pro</h1>
+            <img src="/assets/hero.png" alt="MacBook Pro" />
+        </div>
+        <video src ="/assets/hero-video.mp4" autoPlay muted playsInline />
+    </section>
   )
 }
 
