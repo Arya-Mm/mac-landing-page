9fb6f3e78ca6324c62267d083f7262f18ce4c95e
Arya-Mm <aryamishra828@gmail.com>
2026-03-15 06:26:42 +0000

    Mar 15, 2026, 11:56 AM

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import {useEffect, useRef} from "react";
 
 const Hero = () => {
-  return (
-    <section id = "hero">
-        <div>
-            <h1>MacBook Pro</h1>
-            <img src="/assets/hero.png" alt="MacBook Pro" />
-        </div>
-        <video autoPlay muted loop playsInline>
-  <source src="/assets/hero-video.mp4" type="video/mp4" />
-</video>
-    </section>
-  )
-}
+    const videoRef = useRef();
 
+    useEffect(() => {
+        if(videoRef.current) videoRef.current.playbackRate = 2;
+    }, []);
+
+    return (
+        <section id="hero">
+            <div>
+                <h1>MacBook Pro</h1>
+                <img src="/title.png" alt="MacBook Title" />
+            </div>
+
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+
+            <button>Buy</button>
+
+            <p>From $1599 or $133/mo for 12 months</p>
+        </section>
+    )
+}
 export default Hero
